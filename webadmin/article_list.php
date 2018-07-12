2cc15444510f1bfc3d18fa61894be5379ad73ac4
wanglong <wanglong1002@outlook.com>
2018-07-12 22:55:34 +0800

    后台文章多选删除

--- webadmin/article_list.php
+++ webadmin/article_list.php
@@ -21,6 +21,21 @@ if(isset($_GET['action']))
 				echo "未设置删除ID";
 			}
 		break;
+		case "deleteall":
+			$ids = $_POST['ids'];
+			//echo json_encode($data);
+			$where = "where Id in ('{$ids}')";
+			$rs = $db->deleted("wl_article",$where);
+			if($rs)
+			{
+				echo "success";
+			}
+			else
+			{
+				echo "error";
+			}
+			exit();
+		break;
 	}
 }
 ?>
@@ -37,7 +52,7 @@ if(isset($_GET['action']))
 				<button id="checkall" type="button" class="btn btn-info">
 				 <span class="glyphicon glyphicon-th"></span> 全选
 				</button>
-				<button type="button" class="btn btn-danger">
+				<button id="deleteall" type="button" class="btn btn-danger">
 				 <span class="glyphicon glyphicon-minus"></span> 删除
 				</button>
 			</div>
@@ -97,5 +112,31 @@ $("#checkall").click(function(){
 		$("#checkall").text("取消全选");
 		flag = !flag;
 	}
-})
+});
+$("#deleteall").click(function(){
+	var postModel="";  
+	  //遍历复选框获取要删除的数据ID 存放到数组中  
+	  $("input[name='id']").each(function () {  
+		  if (this.checked) 
+		  {
+			 postModel+=$(this).val()+"','";  
+		  }
+	   });	   
+	 if(postModel.length == 0) {  
+		 alert('请先选择要删除的内容!');  
+		 return;  
+	  }
+	  postModel= postModel.substr(0,postModel.length-3);
+	$.post(
+	"article_list.php?action=deleteall",
+	{"ids":postModel},
+	function(rs){
+		if(rs == "error")
+		{
+			alert("删除失败！");
+		}
+		window.location.reload();
+      }
+	);
+});
 </script>
